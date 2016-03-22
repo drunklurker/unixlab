63abf5a3df7ad05179caebd05df9367ec0305560
fwiffo <adskaya.taburetka@gmail.com>
2016-03-22 03:17:22 +0300

    added parameter parsing for ls

--- src/ls.c
+++ src/ls.c
@@ -2,5 +2,28 @@
 
 int shcmd_ls(char* cmd, char* params[])
 {
+    int params_count = 0;
+    while (params[params_count] != NULL)
+        params_count++;
+    printf("ls started\n");
+    struct ls_struct flags;
+    flags.l = flags.a = flags.i = 0;
+    char opts[] = "lai";
+    int opt;
+    while ((opt = getopt(params_count, params, opts)) != -1)
+    {
+        switch(opt)
+        {
+        case 'l':
+            flags.l = 1;
+            break;
+        case 'a':
+            flags.a = 1;
+            break;
+        case 'i':
+            flags.i = 1;
+            break;
+        }
+    }
 
 }
